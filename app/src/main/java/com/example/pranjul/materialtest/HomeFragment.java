dea552acce84e0de61680b229c54f111e66747c8
LENOVO <singhamitch@gmail.com>
2017-02-03 16:42:05 +0530

    added home activity

--- app/src/main/java/com/example/pranjul/materialtest/HomeFragment.java
+++ app/src/main/java/com/example/pranjul/materialtest/HomeFragment.java
@@ -1,18 +1,11 @@
 package com.example.pranjul.materialtest;
 
-
-import android.content.Context;
-import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.Gallery;
 import android.widget.ImageSwitcher;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -27,9 +20,11 @@ import java.util.Random;
 public class HomeFragment extends Fragment implements ViewSwitcher.ViewFactory {
     private ImageSwitcher imageSwitcher;
     private int images[]={R.drawable.a, R.drawable.b, R.drawable.c, R.drawable.d, R.drawable.e,
-                            R.drawable.f, R.drawable.g, R.drawable.h, R.drawable.i, R.drawable.j};
+                            R.drawable.f, R.drawable.g, R.drawable.h, R.drawable.i, R.drawable.j,
+                            R.drawable.k, R.drawable.l, R.drawable.m, R.drawable.n, R.drawable.o,
+            R.drawable.p, R.drawable.q, R.drawable.r, R.drawable.s};
     //private ThreadClass obj;
-    private int currPos= new Random().nextInt(10);
+    private int currPos= new Random().nextInt(19);
     private boolean isFragmentActive;
     public HomeFragment() {
         // Required empty public constructor
@@ -54,9 +49,9 @@ public class HomeFragment extends Fragment implements ViewSwitcher.ViewFactory {
         imageSwitcher.postDelayed(new Runnable() {
             @Override
             public void run() {
-                int pos=rand.nextInt(10);
+                int pos=rand.nextInt(19);
                 while(pos==currPos)
-                    pos=rand.nextInt(10);
+                    pos=rand.nextInt(19);
                 currPos=pos;
                 imageSwitcher.setImageDrawable(null);
                 imageSwitcher.setImageResource(images[currPos]);
@@ -65,39 +60,6 @@ public class HomeFragment extends Fragment implements ViewSwitcher.ViewFactory {
         }, 4000);
         return layout;
     }
-    /*public class ImageAdapter extends BaseAdapter {
-
-        private Context ctx;
-
-        public ImageAdapter(Context c) {
-            ctx = c;
-        }
-
-        public int getCount() {
-
-            return images.length;
-        }
-
-        public Object getItem(int arg0) {
-
-            return arg0;
-        }
-
-        public long getItemId(int arg0) {
-
-            return arg0;
-        }
-
-        public View getView(int arg0, View arg1, ViewGroup arg2) {
-            ImageView iView = new ImageView(ctx);
-            iView.setScaleType(ImageView.ScaleType.FIT_CENTER);
-            iView.setLayoutParams(new
-                    ImageSwitcher.LayoutParams(
-                    RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT));
-            iView.setImageResource(images[arg0]);
-            return iView;
-        }
-    }*/
 
     @Override
     public View makeView() {
@@ -108,12 +70,4 @@ public class HomeFragment extends Fragment implements ViewSwitcher.ViewFactory {
         return iView;
     }
 
-    /*private class ThreadClass extends AsyncTask<Void, Void, Void> {
-        @Override
-
-        protected Void doInBackground(Void... voids) {
-
-            return null;
-        }
-    */
 }
